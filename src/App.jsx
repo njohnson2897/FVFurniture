9c2c9f59703c2d98bec471fe81f26e5eb345f938
Nate Johnson <njohnson2897@gmail.com>
2024-08-16 13:50:36 -0500

    layout styling

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,15 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <>
+        <div className='border rounded border-dark pt-2 mb-2'>
         <Navbar />
+        </div>
+        <div className='border rounded border-dark pt-2'>
           <Outlet />
+        </div>
+        <div className='border rounded border-dark pt-2 mt-2'>
         <Footer />
+        </div>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,16 @@ import foxImage from '../assets/fox.PNG';
 function Navbar() {
   return (
     <div className='navigationbar pb-1'>
-      <header className="d-flex justify-between items-center mb-2">
-        <img src={foxImage} alt="Image of a fox" height='60'></img>
+      <header className="d-flex justify-content-between mb-2">
+        <div className='d-flex px-3'>
+        <img className='px-3' src={foxImage} alt="Image of a fox" height='60'></img>
         <Link className='text-black' to='/'>
         <h1 className="font-bold text-foreground">
           Fox Valley Furniture
         </h1>
         </Link>
-        <nav className="nav nav-tabs flex space-x-4 mx-5">
+        </div>
+        <nav className="nav nav-tabs align-items-end mx-4">
         <li className='nav-item mx-2'>
           <Link className="text-dark nav-link" to='/'>
             Home
