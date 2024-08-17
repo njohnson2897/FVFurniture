9bee04d1cb7d3e0981da58ee7744df465ef82e0e
Nate Johnson <njohnson2897@gmail.com>
2024-08-16 20:13:09 -0500

    adds layout and filler to about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,30 +1,38 @@
+import placeholder from '../assets/placeholder.PNG';
+
 function AboutUsPage() {
-    return (
-        <div className='container'>
-        <h1>About Us</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste dolore nobis aspernatur maxime sunt? Fugit soluta autem ipsum eligendi nisi hic quasi iusto laborum assumenda! Quod, quae doloribus? Reiciendis, nobis.
-        </p>
+  return (
+    <div>
+      <h1>About Us</h1>
+      <div className='container d-flex'>
+      <div className="column mx-5">
+        <div className="row border-bottom my-3">
+            <h2>Our Mission</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi!</p>
         </div>
-    );
+        <div className="row my-3">
+        <img className='px-3' src={placeholder} alt="" height='300'></img>
+        </div>
+        <div className="row border-top my-3">
+            <h2>FAQs</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error recusandae, minima quisquam facere unde numquam libero repellat molestiae aliquam reiciendis beatae, quod veritatis. Voluptatem suscipit maiores deleniti? Dolorum, consectetur doloribus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae amet, fugit accusantium consequuntur eum maiores ad laboriosam, ratione magnam nihil necessitatibus, officiis placeat quidem quod consectetur minus illum doloremque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, doloribus. Exercitationem, dicta id. Necessitatibus maiores quae suscipit nulla, iure quia dolores magnam non? Iusto dolorum dolores aspernatur expedita, ad sunt.</p>
+        </div>
+      </div>
+      <div className="column">
+        <div className="row border-bottom py-4 my-3">
+        <img className='px-3' src={placeholder} alt="" height='300'></img>
+        </div>
+        <div className="row my-3">
+            <h2>Who We Are</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus sunt accusamus iusto accusantium? Aliquam reprehenderit fuga neque facilis nihil iste officia, inventore aliquid dolor, magnam consequatur, dolores autem. Provident, sequi!</p>
+        </div>
+        <div className="row border-top py-4 my-3">
+        <img className='px-3' src={placeholder} alt="" height='300'></img>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUsPage;
